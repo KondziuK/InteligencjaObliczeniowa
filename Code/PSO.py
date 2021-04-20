068059495b07dbc2c808af26bcd574242c135eac
Piotr Wzorek <pwz@semihalf.com>
2021-04-20 20:48:25 +0200

    Cos probujemy, cos dziala, cos nie dziala

--- Code/PSO.py
+++ Code/PSO.py
@@ -14,7 +14,7 @@ class PSO:
                  max_iter_before_explosion:int, max_iter_without_gbest_update: int,max_iter_before_termination: int):
 
         self.gbest_value = 1000000
-        self.gbest_position = [0, 0, 1, 0]
+        self.gbest_position = [0, 0]
         self.W = W
         self.c1 = C1
         self.c2 = C2
@@ -22,12 +22,8 @@ class PSO:
         self.ref_image = ref_image
         self.landscape = landscape
         self.nb_of_channels = ref_image.shape[-1]
-        self.max_x, self.max_y = landscape.shape[:2]
+        self.max_y, self.max_x = landscape.shape[:2]
         self.min_x, self.min_y = [0,0]
-        self.min_s = 0.5
-        self.max_s = 2
-        self.min_r = -180
-        self.max_r = 180
         self.particles = self.init_with_random_values()
         self.iteration = 0
         self.iteration_since_gbest_update = 0
@@ -50,19 +46,15 @@ class PSO:
         :return: List of particles
         """
         particles = []
-        min_x = self.min_x + self.ref_image.shape[0] / 2
-        min_y = self.min_y + self.ref_image.shape[1] / 2
-        max_x = self.max_x - self.ref_image.shape[0] / 2
-        max_y = self.max_y - self.ref_image.shape[1] / 2
+        min_x = int(self.min_x + (self.ref_image.shape[1]/2)+1)
+        min_y = int(self.min_y + (self.ref_image.shape[0]/2)+1)
+        max_x = int(self.max_x - (self.ref_image.shape[1]/2)-1)
+        max_y = int(self.max_y - (self.ref_image.shape[0]/2)-1)
+        print(min_x, min_y, max_x, max_y)
         for i in range(0, self.nb_of_particles):
             x = random.randint(int(min_x), int(max_x))
             y = random.randint(int(min_y), int(max_y))
-            s = random.uniform(self.min_s, self.max_s) #WOLIMY MNIEJ MIJESC PO PRZECINKU - OGARNIJMY TO
-            r = random.randint(int(self.min_r), int(self.max_r))
-            print("START")
-            print((x,y,s,r))
-            particles.append(Particle(x, y, s, r))
-
+            particles.append(Particle(x, y))
         return particles
 
     def update_particles_velocity(self) -> None:
@@ -70,65 +62,24 @@ class PSO:
         r2 = random.uniform(0, 1)
         for particle in self.particles:
             velocity = (self.W * particle.velocity) + (self.c1 * r1 * (particle.pbest_position - particle.position)) + (self.c2 * r2 * (self.gbest_position - particle.position))
-            for i in range(0,4):
-                if velocity[i] > 20:
-                    velocity[i] = 20
-                if velocity[i] < -20:
-                    velocity[i] = -20
             particle.update_velocity(velocity)
-        print("\n\nUPDATE")
-        print(velocity)
 
     def update_particles_position(self) -> None:
-        min_x = int(self.min_x + self.ref_image.shape[0] / 2)
-        min_y = int(self.min_y + self.ref_image.shape[1] / 2)
-        max_x = int(self.max_x - self.ref_image.shape[0] / 2)
-        max_y = int(self.max_y - self.ref_image.shape[1] / 2)
+        min_x = int(self.min_x + (self.ref_image.shape[1]/2)+1)
+        min_y = int(self.min_y + (self.ref_image.shape[0]/2)+1)
+        max_x = int(self.max_x - (self.ref_image.shape[1]/2)-1)
+        max_y = int(self.max_y - (self.ref_image.shape[0]/2)-1)
         for particle in self.particles:
-            position = np.zeros(4)
-            if min_x > int(particle.position[0] + particle.velocity[0]):
-                print("MIN")
-                position[0] = int(min_x + (min_x - particle.position[0] - particle.velocity[0]))
-            elif max_x < particle.position[0] + particle.velocity[0]:
-                print("MAX")
-                position[0] = int(max_x - particle.velocity[0] + (max_x - particle.position[0]))
-            else:
-                print("NONE")
+            position = np.zeros(2)
+            if min_x < int(particle.position[0] + particle.velocity[0]) < max_x:
                 position[0] = int(particle.position[0] + particle.velocity[0])
-
-            if min_y > int(particle.position[1] + particle.velocity[1]):
-                position[1] = int(min_y + (min_y - particle.position[1] - particle.velocity[1]))
-            elif max_y < particle.position[1] + particle.velocity[1]:
-                position[1] = int(max_y - particle.velocity[1] + (max_y - particle.position[1]))
             else:
+                position[0] = random.randint(int(min_x), int(max_x))
+            if min_y < int(particle.position[1] + particle.velocity[1]) < max_y:
                 position[1] = int(particle.position[1] + particle.velocity[1])
-
-            # if self.min_s > particle.position[2] + particle.velocity[2]:
-            #     position[2] = - (particle.position[2] + particle.velocity[2])
-            # elif self.max_s < particle.position[2] + particle.velocity[2]:
-            #     position[2] = self.max_s - particle.velocity[2] + (self.max_s - particle.position[2])
-            # else:
-            #     position[2] = particle.position[2] + particle.velocity[2]
-            position[2] = 1
-
-            if self.min_r > particle.position[3] + particle.velocity[3]:
-                position[3] = - (particle.position[3] + particle.velocity[3])
-            elif self.max_r < particle.position[3] + particle.velocity[3]:
-                position[3] = self.max_s - particle.velocity[3] + (self.max_s - particle.position[3])
             else:
-                position[3] = particle.position[3] + particle.velocity[3]
-            print(position)
-            if min_x <= position[0] <= max_x:
-                print("OKEJ")
-            else:
-                print(position[0])
-                sys.exit(11)
+                position[1] = random.randint(int(min_y), int(max_y))
 
-            if min_y <= position[1] <= max_y:
-                print("OKEJ")
-            else:
-                print(min_y)
-                sys.exit(12)
             particle.update_position(position)
 
     def evaluate_fitness(self) -> None:
@@ -140,29 +91,28 @@ class PSO:
         self.iteration += 1
 
         for particle in self.particles:
-            x, y, s, tau = particle.position
+            x, y = particle.position
             Pinv = 0 ## ??????????????????????
             nbits = 8
-            m, n = self.ref_image.shape[:2]
+            n, m = self.ref_image.shape[:2]
             error_calc = 0
-            for i in range (0, n):
-                for j in range (0, m):
-                    ddX = j - m/2
-                    ddY = i - n/2
+            for i in range(0,n):
+                for j in range(0,m):
+                    # ddX = j - m/2
+                    # ddY = i - n/2
                     # I = int(y + s * (ddX * math.sin(-tau * (math.pi / 180)) + ddY * math.cos(tau * (math.pi / 180))))
                     # J = int(x + s * (ddX * math.cos(-tau * (math.pi / 180)) + ddY * math.sin(tau * (math.pi / 180))))
-                    I = int(x + i)
-                    J = int(y + j)
+                    I = int(y + i - n/2)
+                    J = int(x + j - m/2)
                     ## Calculating error calc and checking if indexes works well
                     try:
-                        error_calc += abs(self.ref_image[j, i] - self.landscape[J, I])
+                        error_calc += abs(self.ref_image[i, j] - self.landscape[I, J])
                     except IndexError:
                         error_calc += 255
-                        return 10000000000000000
-                        # print(f"IndexError in iteration{self.iteration} for position {x}, {y}"
-                        #       f"for i = {i}, j = {j},"
-                        #       f"for I = {I}, J = {J}")
-
+                        print(f"IndexError in iteration{self.iteration} for position {x}, {y}"
+                               f"for i = {j}, j = {i},"
+                               f"for I = {J}, J = {I}")
+                        sys.exit(7)
 
             err_max = (2 ** nbits) * ((m * n) - Pinv)
 
@@ -176,15 +126,13 @@ class PSO:
             ## Updating gbest, pbest if needed
             if error < self.gbest_value:
                 self.gbest_value = error
-                self.gbest_position = [x, y, s, tau * (math.pi / 180)]
+                self.gbest_position = [x, y]
                 self.iteration_since_gbest_update = 0 ## Zeroing iterations since gbest update
-
+            else:
+                self.iteration_since_gbest_update = self.iteration_since_gbest_update + 1
 
             if error < particle.pbest_value:
-                particle.update_pbest(error, [x, y, s, tau * (math.pi / 180)])
-
-
-
+                particle.update_pbest(error, [x, y])
 
 
     def explode(self) -> None:
@@ -193,13 +141,16 @@ class PSO:
         :return:
         """
         for particle in self.particles:
-
-            x = random.randint(0, self.landscape.shape[1] - self.ref_image.shape[1])
-            y = random.randint(0, self.landscape.shape[0] - self.ref_image.shape[0])
-            s = random.randrange(0, 2)
-            tau = 0 # for now
-            particle.position = np.array([x, y, s, tau * (math.pi / 180)])
-            # velocity tez zmieniamy?
+            position = np.zeros(2)
+            min_x = int(self.min_x + (self.ref_image.shape[1]/2) + 1)
+            min_y = int(self.min_y + (self.ref_image.shape[0]/2) + 1)
+            max_x = int(self.max_x - (self.ref_image.shape[1]/2) - 1)
+            max_y = int(self.max_y - (self.ref_image.shape[0]/2) - 1)
+            position[0] = random.randint(int(min_x), int(max_x))
+            position[1] = random.randint(int(min_y), int(max_y))
+            particle.update_position(position)
+            particle.update_pbest(0, [0, 0])
+        print("EXPLODE")
 
 
     def should_terminate(self) -> bool:
@@ -212,9 +163,11 @@ class PSO:
         if self.iteration >= self.max_iter_before_termination:
             print("MAX ITER REACHED, ACHIEVED")
             return True
-        elif self.iteration_since_gbest_update >= self.max_iter_without_gbest_update:
-            print("MAX ITER WITHOUT G_BEST REACHED, ACHIEVED")
-            return True
+        # TODO: Konniec jak za długo nie zmienia się gbest
+        elif self.iteration_since_gbest_update >= self.max_iter_before_explosion:
+            self.explode()
+            self.iteration_since_gbest_update = 0 # potem to wywalić
+            return False
         else:
             return False
 
@@ -222,14 +175,13 @@ class Particle:
 
     # TO DO : Update_position, update_velocity, update_pbest etc
 
-    def __init__(self, x: int, y: int, s: float, tau: float):
+    def __init__(self, x: int, y: int):
 
         tau = 0
-        self.position = np.array([x, y, s, tau*(math.pi/180)])
-        self.velocity = np.array([random.uniform(-0.1, 0.1) for i in range(0, 4)])
+        self.position = np.array([x, y])
+        self.velocity = np.array([random.uniform(-0.1, 0.1) for i in range(0, 2)])
         self.pbest_position = copy.deepcopy(self.position)
         self.pbest_value = 1000000
-        print(self.velocity)
 
     def update_position(self, position: np.array):
         self.position = position
@@ -249,9 +201,9 @@ def main():
     C1 = 2
     C2 = 2
     nb_of_particles = 20
-    max_iter_before_explosion = 15
+    max_iter_before_explosion = 10*nb_of_particles
     max_iter_without_gbest_update = 20
-    max_iter_before_termination = 500
+    max_iter_before_termination = 50
     reference_image_path = "shapes_pat.png"
     landscape_image_path = "shapes.jpg"
     #######################################################
@@ -265,7 +217,12 @@ def main():
 
     start_time = time.time()
     count = 0
+    show_me = landImage.copy()
     while not pso.should_terminate():# ??kolejność plus co gdzie powinno byc wywoływane?
+        for particle in pso.particles:
+            cv2.circle(show_me, (int(particle.position[0]), int(particle.position[1])), radius=0, color=(0, 0, 0), thickness=6)
+        # cv2.imshow("Image", show_me)
+        # cv2.waitKey()
         pso.evaluate_fitness()
         pso.update_particles_velocity()
         pso.update_particles_position()
@@ -274,16 +231,14 @@ def main():
 
     end_time = time.time()
 
-    #draw rectangle # TODO: dodac skalowanie prostokata
-    (refImage.shape[0] * pso.gbest_position[2])/2
-    start_point = (int(pso.gbest_position[1] - (refImage.shape[1] * pso.gbest_position[2])/2), int(pso.gbest_position[0] - (refImage.shape[0] * pso.gbest_position[2])/2))
-    end_point = (int(pso.gbest_position[1] + (refImage.shape[1] * pso.gbest_position[2])/2), int(pso.gbest_position[0] + (refImage.shape[0] * pso.gbest_position[2])/2))
+    start_point = (int(pso.gbest_position[0] - (refImage.shape[1])/2), int(pso.gbest_position[1] - (refImage.shape[0])/2))
+    end_point = (int(pso.gbest_position[0] + (refImage.shape[1])/2), int(pso.gbest_position[1] + (refImage.shape[0])/2))
     color = (68, 104, 124)
     thickness = 3
 
-    cv2.rectangle(landImage, start_point, end_point, color, thickness)
+    cv2.rectangle(show_me, start_point, end_point, color, thickness)
 
-    cv2.imshow('landscape', landImage)
+    cv2.imshow('landscape', show_me)
     cv2.imshow("refImage", refImage)
 
     print(f"elapsed time: {end_time - start_time} s")
